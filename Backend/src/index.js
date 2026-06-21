ff2ac51096aba1420ea365ce852c184a59daaf7b
Rohan Kamat <rohankamat7986@gmail.com>
2026-06-21 12:47:19 +0530

    connected more dependicies in file

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
 import dns from "dns";
+import { app } from "./app.js";
 
 dns.setServers([
   '1.1.1.1',
@@ -14,3 +15,11 @@ dotenv.config({
 
 
 connectDb()
+.then(() => {
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`Server is Running at Port: ${process.env.PORT}`)
+  })
+})
+.catch((err) =>{
+  console.log("MONGDB connection failed !!!", err);
+})
