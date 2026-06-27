7daa09900dacf3255ebf5a118914d7119bf59025
Rohan Kamat <rohankamat7986@gmail.com>
2026-06-27 11:17:55 +0530

    feat: add new routes and controllers for tweets, comments, likes, subscriptions, playlists, and dashboard

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,23 +1,44 @@
 import { Router } from "express";
-import { registerUser, loginUser, logOut } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
+import { 
+    loginUser, 
+    logout, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    updateAccountDetails
+} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 
 const router = Router()
 
 router.route("/register").post(
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1
-    }
-  ]),
-  registerUser
-);
+    upload.field([
+        {
+            name: "avatar",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
 
 router.route("/login").post(loginUser)
 
 //secured routes
-router.route("/logout").post(verifyJWT, logOut);
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
-export default router;
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+export default router
