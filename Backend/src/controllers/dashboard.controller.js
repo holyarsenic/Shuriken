8c4dc68b172b2af1cd79750669ed9b99cd21185d
Rohan Kamat <rohankamat7986@gmail.com>
2026-06-29 12:35:49 +0530

    feat: implement channel statistics and posts retrieval with aggregation and user subscription details

--- Backend/src/controllers/dashboard.controller.js
+++ Backend/src/controllers/dashboard.controller.js
@@ -8,10 +8,225 @@ import {asyncHandler} from "../utils/asynchandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+      const channelId = req.user?._id
+
+    // total posts
+    const totalposts = await Post.countDocuments({
+        owner: channelId
+    })
+
+    // total video views
+    const totalViewsResult = await Post.aggregate([
+
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+
+        {
+            $group: {
+                _id: null,
+                totalViews: {
+                    $sum: "$views"
+                }
+            }
+        }
+
+    ])
+
+    const totalViews =
+        totalViewsResult[0]?.totalViews || 0
+
+    // total subscribers
+    const totalSubscribers = await Subscription.countDocuments({
+        channel: channelId
+    })
+
+    // total likes on posts
+    const totalLikesResult = await Post.aggregate([
+
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "post",
+                as: "likes"
+            }
+        },
+
+        {
+            $addFields: {
+                likesCount: {
+                    $size: "$likes"
+                }
+            }
+        },
+
+        {
+            $group: {
+                _id: null,
+                totalLikes: {
+                    $sum: "$likesCount"
+                }
+            }
+        }
+
+    ])
+
+    const totalLikes =
+        totalLikesResult[0]?.totalLikes || 0
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                totalposts,
+                totalViews,
+                totalSubscribers,
+                totalLikes
+            },
+            "Channel stats fetched successfully"
+        )
+    )
 })
 
 const getChannelPosts = asyncHandler(async (req, res) => {
     // TODO: Get all the posts uploaded by the channel
+     const channelPosts = await Post.aggregate([
+
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(
+                    req.user?._id
+                )
+            }
+        },
+
+        // likes
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "post",
+                as: "likes"
+            }
+        },
+
+        {
+            $addFields: {
+
+                likesCount: {
+                    $size: "$likes"
+                }
+
+            }
+        },
+
+        // owner
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+
+                pipeline: [
+
+                    {
+                        $lookup: {
+                            from: "subscriptions",
+                            localField: "_id",
+                            foreignField: "channel",
+                            as: "subscribers"
+                        }
+                    },
+
+                    {
+                        $addFields: {
+
+                            subscribersCount: {
+                                $size: "$subscribers"
+                            },
+
+                            isSubscribed: {
+                                $cond: {
+                                    if: {
+                                        $in: [
+                                            req.user?._id,
+                                            "$subscribers.subscriber"
+                                        ]
+                                    },
+                                    then: true,
+                                    else: false
+                                }
+                            }
+
+                        }
+                    },
+
+                    {
+                        $project: {
+                            fullName: 1,
+                            username: 1,
+                            avatar: 1,
+                            subscribersCount: 1,
+                            isSubscribed: 1
+                        }
+                    }
+
+                ]
+            }
+        },
+
+        {
+            $addFields: {
+                owner: {
+                    $first: "$owner"
+                }
+            }
+        },
+
+        {
+            $project: {
+                postFile: 1,
+                title: 1,
+                description: 1,
+                duration: 1,
+                views: 1,
+                createdAt: 1,
+                isPublished: 1,
+                likesCount: 1,
+                owner: 1
+            }
+        },
+
+        {
+            $sort: {
+                createdAt: -1
+            }
+        }
+
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            channePosts,
+            "Channel posts fetched successfully"
+        )
+    )
+
 })
 
 export {
