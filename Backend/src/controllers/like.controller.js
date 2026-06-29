e74e3a0a3bb2cc25dd993906610086b505eb561c
Rohan Kamat <rohankamat7986@gmail.com>
2026-06-29 12:42:20 +0530

    feat: implement like functionality for posts, comments, and tweets with error handling and response messages

--- Backend/src/controllers/like.controller.js
+++ Backend/src/controllers/like.controller.js
@@ -5,24 +5,274 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asynchandler.js"
 
 const togglePostLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
-    //TODO: toggle like on video
+    const {postId} = req.params
+    //TODO: toggle like on post
+     if (!isValidObjectId(postId)) {
+        throw new ApiError(400, "Invalid post id")
+    }
+
+    const post = await Post.findById(postId)
+
+    if (!post) {
+        throw new ApiError(404, "Post not found")
+    }
+
+    const alreadyLiked = await Like.findOne({
+        post: postId,
+        likedBy: req.user?._id
+    })
+
+    // unlike
+    if (alreadyLiked) {
+
+        await Like.findByIdAndDelete(
+            alreadyLiked._id
+        )
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Post unliked successfully"
+            )
+        )
+    }
+
+    // like
+    const like = await Like.create({
+        post: postId,
+        likedBy: req.user?._id
+    })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            like,
+            "Post liked successfully"
+        )
+    )
+
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const {commentId} = req.params
     //TODO: toggle like on comment
 
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment id")
+    }
+
+    const comment = await Comment.findById(commentId)
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found")
+    }
+
+    const alreadyLiked = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user?._id
+    })
+
+    // unlike
+    if (alreadyLiked) {
+
+        await Like.findByIdAndDelete(
+            alreadyLiked._id
+        )
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Comment unliked successfully"
+            )
+        )
+    }
+
+    // like
+    const like = await Like.create({
+        comment: commentId,
+        likedBy: req.user?._id
+    })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            like,
+            "Comment liked successfully"
+        )
+    )
+
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
     //TODO: toggle like on tweet
-}
-)
+        const { tweetId } = req.params
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+    const alreadyLiked = await Like.findOne({
+        tweet: tweetId,
+        likedBy: req.user?._id
+    })
+
+    // unlike
+    if (alreadyLiked) {
+
+        await Like.findByIdAndDelete(
+            alreadyLiked._id
+        )
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                {},
+                "Tweet unliked successfully"
+            )
+        )
+    }
+
+    // like
+    const like = await Like.create({
+        tweet: tweetId,
+        likedBy: req.user?._id
+    })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            like,
+            "Tweet liked successfully"
+        )
+    )
+})
 
 const getLikedPosts = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    //TODO: get all liked posts
+        const likedPosts = await Like.aggregate([
+
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(
+                    req.user?._id
+                ),
+                post: {
+                    $exists: true
+                }
+            }
+        },
+
+        {
+            $lookup: {
+                from: "posts",
+                localField: "post",
+                foreignField: "_id",
+                as: "post",
+
+                pipeline: [
+
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+
+                            pipeline: [
+
+                                {
+                                    $lookup: {
+                                        from: "subscriptions",
+                                        localField: "_id",
+                                        foreignField: "channel",
+                                        as: "subscribers"
+                                    }
+                                },
+
+                                {
+                                    $addFields: {
+                                        subscribersCount: {
+                                            $size: "$subscribers"
+                                        },
+
+                                        isSubscribed: {
+                                            $cond: {
+                                                if: {
+                                                    $in: [
+                                                        req.user?._id,
+                                                        "$subscribers.subscriber"
+                                                    ]
+                                                },
+                                                then: true,
+                                                else: false
+                                            }
+                                        }
+                                    }
+                                },
+
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1,
+                                        subscribersCount: 1,
+                                        isSubscribed: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        },
+
+        {
+            $addFields: {
+                post: {
+                    $first: "$post"
+                }
+            }
+        }
+
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            likedPosts,
+            "Liked posts fetched successfully"
+        )
+    )
 })
 
 export {
