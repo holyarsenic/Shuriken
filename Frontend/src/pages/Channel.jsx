f944abaaa451fb7a949d680698feb8547dfa0108
Rohan Kamat <rohankamat7986@gmail.com>
2026-07-19 12:58:39 +0530

    style: adjust loading spinner size and background color for consistency in ProtectedRoute and Channel components

--- Frontend/src/pages/Channel.jsx
+++ Frontend/src/pages/Channel.jsx
@@ -25,7 +25,7 @@ const Channel = () => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-white dark:bg-[#0B0A10]">
         <div className="flex">
-          <span className="w-10 h-10 rounded-full border-4 border-slate-300 dark:border-slate-600 border-t-violet-500 dark:border-t-violet-500 animate-spin" />
+          <span className="w-7 h-7 lg:w-10 lg:h-10 rounded-full border-4 border-slate-300 dark:border-slate-600 border-t-violet-500 dark:border-t-violet-500 animate-spin" />
         </div>
       </div>
     );
